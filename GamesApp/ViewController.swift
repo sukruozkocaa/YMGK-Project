da8e264047ecc6ecbc81176c7ec29cbc038abf28
sukruozkocaa <sukruozkocaa19@gmail.com>
2023-04-03 00:51:35 +0300

    Initial Commit

---
+++ GamesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GamesApp
+//
+//  Created by Şükrü Özkoca on 3.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
